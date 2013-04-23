20d3f3d7e76204b80190befd42f2e1717835be69
Aili Madisson <aili.madisson@gmail.com>
2013-04-23 14:47:10 +0300

    language links to navigation bar

---
+++ template.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * Provides preprocess logic and other functionality for theming.
+ */
+
+/**
+ * Implements hook_preprocess_page().
+ */
+function edidaktikum_theme_preprocess_page(&$vars) {
+  $page = $vars['page'];
+  $vars['navbar_languages'] = FALSE;
+  if (module_exists('locale')) {
+    $languages_block = module_invoke('locale', 'block_view', 'language');
+    $vars['navbar_languages'] = $languages_block['content'];
+  }
+}
+
