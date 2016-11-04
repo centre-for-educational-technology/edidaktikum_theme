c94de776b9afe1201c0a921c371d0d8864465299
GlebRed <glebredko@gmail.com>
2016-11-04 15:19:08 +0200

    Closes centre-for-educational-technology/edidaktikum#450 parallax introduced

--- templates/page--front.tpl.php
+++ templates/page--front.tpl.php
@@ -11,171 +11,163 @@ global $user;
 <div id="wrap">
 
 
-<!-- Navbar -->
-<div id="navbar" class="navbar navbar-static-top">
-  <div class="navbar-inner">
-    <div class="container">
+  <!-- Navbar -->
+  <div id="navbar" class="navbar navbar-static-top">
+    <div class="navbar-inner">
+      <div class="container">
 
-    <?php print $navbar_toggler ?>
-    <?php print $navbar_brand ?>
+      <?php print $navbar_toggler ?>
+      <?php print $navbar_brand ?>
 
-    <?php if (user_is_logged_in()): ?>
-      <div class="brand brand-avatar">
-        <?php
-        $user_picture_data = array('account' => $user);
-        template_preprocess_user_picture($user_picture_data);
-        print $user_picture_data['user_picture'];
-        ?>
+      <?php if (user_is_logged_in()): ?>
+        <div class="brand brand-avatar">
+          <?php
+          $user_picture_data = array('account' => $user);
+          template_preprocess_user_picture($user_picture_data);
+          print $user_picture_data['user_picture'];
+          ?>
+        </div>
+      <?php endif; ?>
+
+      <?php print $navbar_search ?>
+      <?php if ($navbar_languages): ?><?php print $navbar_languages ?><?php endif ?>
+      <?php if ($navbar_menu): ?>
+        <nav class="nav-collapse collapse" role="navigation">
+          <?php print $navbar_menu ?>
+        </nav>
+      <?php endif ?>
       </div>
-    <?php endif; ?>
-
-    <?php print $navbar_search ?>
-    <?php if ($navbar_languages): ?><?php print $navbar_languages ?><?php endif ?>
-    <?php if ($navbar_menu): ?>
-      <nav class="nav-collapse collapse" role="navigation">
-        <?php print $navbar_menu ?>
-      </nav>
-    <?php endif ?>
     </div>
   </div>
-</div>
 
 
 
-
-<section class="front-image-section">
-  <div class="container front-page-image">
-
-
-
-    <article class="front-calltoaction row">
-
-        <h1 class="headline">
-        <?php print t('Didaktikavaramu ja õpetajakoolituse keskkond') ?>
-        </h1>
-        <p class="call-to-action">
-<!--          <a href="#" class="btn btn-large btn-primary rounded">Logi sisse</a>-->
-          <?php
-          if (!$user->uid) {
-            print l(t('Sign in'), 'user', array('attributes' => array('class' => 'btn btn-large btn-info rounded')));
-          }
-          ?>
-          <?php
-          if (!$user->uid) {
-            print l(t('Register'), 'user/register', array('attributes' => array('class' => 'btn btn-large btn-success rounded')));
-          }
-          ?>
-          <?php
-          if ($user->uid) {
-            print l(t('Dashboard'), 'dashboard', array('attributes' => array('class' => 'btn btn-large btn-success rounded')));
-          }
-          ?>
-        </p>
+  <!--Parallax full screen photo-->
+  <section class="front-image-section">
+    <div class="container front-page-image">
 
 
 
-    </article>
-  </div>
-</section>
+      <article class="front-calltoaction row" id="front-hello">
+
+          <h1 class="headline">
+          <?php print t('Didaktikavaramu ja õpetajakoolituse keskkond') ?>
+          </h1>
+          <p class="call-to-action">
+            <?php
+            if (!$user->uid) {
+              print l(t('Sign in'), 'user', array('attributes' => array('class' => 'btn btn-large btn-info rounded')));
+            }
+            ?>
+            <?php
+            if (!$user->uid) {
+              print l(t('Register'), 'user/register', array('attributes' => array('class' => 'btn btn-large btn-success rounded')));
+            }
+            ?>
+            <?php
+            if ($user->uid) {
+              print l(t('Dashboard'), 'dashboard', array('attributes' => array('class' => 'btn btn-large btn-success rounded')));
+            }
+            ?>
+          </p>
 
 
 
-<!-- Marketing messaging and featurettes
-================================================== -->
-<!-- Wrap the rest of the page in another container to center all the content. -->
-
-<div class="container front-page">
-
-
-  <!-- Three columns of text below the carousel -->
-  <div class="row">
-    <div class="span12 heading">
-      <h2><?php print t('What is eDidaktikum?'); ?></h2>
-      <p><?php print $what_text ?></p>
+      </article>
     </div>
-    <div class="span4">
-      <i class="fa fa-5x fa-dashboard"></i>
-      <h2><?php print t('Dashboard'); ?></h2>
-      <p><?php print $dashboard_text ?></p>
-      <p><a class="btn" href="#">View details »</a></p>
-    </div><!-- /.span4 -->
-    <div class="span4">
-      <i class="fa fa-5x fa-users"></i>
-      <h2><?php print t('Groups and Courses'); ?></h2>
-      <p><?php print $groups_text ?></p>
-      <p><a class="btn" href="#">View details »</a></p>
-    </div><!-- /.span4 -->
-    <div class="span4">
-      <i class="fa fa-5x fa-star"></i>
-      <h2><?php print t('Collections'); ?></h2>
-      <p><?php print $collections_text ?></p>
-      <p><a class="btn" href="#">View details »</a></p>
-    </div><!-- /.span4 -->
-  </div><!-- /.row -->
-
-
-  <!-- START THE FEATURETTES -->
-
-  <hr class="featurette-divider">
-
-  <section class="row">
-    <div class="span12">
-      <div class="flex-video widescreen"><iframe src="https://www.youtube.com/embed/nr-G27pQohM" frameborder="0" allowfullscreen=""></iframe></div>
-
-    </div>
-
-
   </section>
 
 
-  <hr class="featurette-divider">
+  <div class="main">
+    <div class="container front-page">
 
-  <!-- Three columns of text below the carousel -->
-  <div class="row">
-    <div class="span12 heading">
-      <h2>eDidaktikum numbrites</h2>
-    </div>
-    <div class="span4">
-      <i class="fa fa-5x fa-smile-o"></i>
-      <p><?php print $teachers_count ?> õppejõudu ja <?php print $students_count ?> tudengit</p>
-    </div><!-- /.span4 -->
-    <div class="span4">
-      <i class="fa fa-5x fa-book"></i>
-      <p><?php print $groups_count ?> kursust ja gruppi <?php print $res_count ?> õppematerjali</p>
-    </div><!-- /.span4 -->
-    <div class="span4">
-      <i class="fa fa-5x fa-files-o"></i>
-      <p><?php print $tasks_count ?> antud ülesannet</p>
-    </div><!-- /.span4 -->
-  </div><!-- /.row -->
-  <hr class="featurette-divider">
 
-  <!-- /END THE FEATURETTES -->
+      <!-- Three columns of text below the photo -->
+      <div class="row">
+        <div class="span12 heading">
+          <h2><?php print t('What is eDidaktikum?'); ?></h2>
+          <p><?php print $what_text ?></p>
+        </div>
+        <div class="span4">
+          <i class="fa fa-5x fa-dashboard"></i>
+          <h2><?php print t('Dashboard'); ?></h2>
+          <p><?php print $dashboard_text ?></p>
+          <p><a class="btn" href="dashboard"><?php print t('Dashboard'); ?> »</a></p>
+        </div><!-- /.span4 -->
+        <div class="span4">
+          <i class="fa fa-5x fa-users"></i>
+          <h2><?php print t('Groups and Courses'); ?></h2>
+          <p><?php print $groups_text ?></p>
+          <p><a class="btn" href="clusters"><?php print t('Groups'); ?> »</a></p>
+        </div><!-- /.span4 -->
+        <div class="span4">
+          <i class="fa fa-5x fa-star"></i>
+          <h2><?php print t('Collections'); ?></h2>
+          <p><?php print $collections_text ?></p>
+          <p><a class="btn" href="collections"><?php print t('Collections'); ?> »</a></p>
+        </div><!-- /.span4 -->
+      </div><!-- /.row -->
 
-  <div class="row">
-    <div class="span12 contact">
-      <div class="hero-unit">
-        <h2><?php print t('Contact Us') ?></h2>
-        <br>
-        <?php print $contacts_text ?>
+
+      <hr class="featurette-divider">
+
+      <section class="row">
+        <div class="span12">
+          <div class="flex-video widescreen"><iframe src="https://www.youtube.com/embed/nr-G27pQohM" frameborder="0" allowfullscreen=""></iframe></div>
+        </div>
+      </section>
+
+
+      <hr class="featurette-divider">
+
+      <!-- Three columns of statistics -->
+      <div class="row">
+        <div class="span12 heading">
+          <h2>eDidaktikum numbrites</h2>
+        </div>
+        <div class="span4">
+          <i class="fa fa-5x fa-smile-o"></i>
+          <p><?php print $teachers_count ?> õppejõudu ja <?php print $students_count ?> tudengit</p>
+        </div><!-- /.span4 -->
+        <div class="span4">
+          <i class="fa fa-5x fa-book"></i>
+          <p><?php print $groups_count ?> kursust ja gruppi <?php print $res_count ?> õppematerjali</p>
+        </div><!-- /.span4 -->
+        <div class="span4">
+          <i class="fa fa-5x fa-files-o"></i>
+          <p><?php print $tasks_count ?> antud ülesannet</p>
+        </div><!-- /.span4 -->
+      </div><!-- /.row -->
+      <hr class="featurette-divider">
+
+
+      <!--Contacts-->
+      <div class="row">
+        <div class="span12 contact">
+          <div class="hero-unit">
+            <h2><?php print t('Contact Us') ?></h2>
+            <br>
+            <?php print $contacts_text ?>
+          </div>
+        </div>
+      </div><!-- /.row -->
+
+
+      <!-- Partners -->
+      <div class="row">
+        <div class="span12">
+          <div id="logos-text">
+            <?php print $logos_text ?>
+          </div>
+
+        </div>
       </div>
-    </div>
-  </div><!-- /.row -->
 
+    </div><!-- /.container -->
 
-  <!-- FOOTER -->
-  <div class="row">
-    <div class="span12">
-      <div id="logos-text">
-        <?php print $logos_text ?>
-      </div>
+  </div><!-- /.main -->
 
-    </div>
-  </div>
-
-</div><!-- /.container -->
-
-</div>
+</div><!-- /.wrap -->
 
   <!-- Footer -->
 <footer id="footer">
@@ -198,29 +190,3 @@ global $user;
     <?php print $copyright ?>
   </div>
 </footer>
-
-
-
-
-
-
-<!-- Le javascript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<!--<script src="../assets/js/jquery.js"></script>-->
-<!--<script src="../assets/js/bootstrap-transition.js"></script>-->
-<!--<script src="../assets/js/bootstrap-alert.js"></script>-->
-<!--<script src="../assets/js/bootstrap-modal.js"></script>-->
-<!--<script src="../assets/js/bootstrap-dropdown.js"></script>-->
-<!--<script src="../assets/js/bootstrap-scrollspy.js"></script>-->
-<!--<script src="../assets/js/bootstrap-tab.js"></script>-->
-<!--<script src="../assets/js/bootstrap-tooltip.js"></script>-->
-<!--<script src="../assets/js/bootstrap-popover.js"></script>-->
-<!--<script src="../assets/js/bootstrap-button.js"></script>-->
-<!--<script src="../assets/js/bootstrap-collapse.js"></script>-->
-<!--<script src="../assets/js/bootstrap-carousel.js"></script>-->
-<!--<script src="../assets/js/bootstrap-typeahead.js"></script>-->
-
-<!--<script src="../assets/js/holder/holder.js"></script>-->
-
-
